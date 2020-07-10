02163af44b62ca1b5f9cf51be22d2c05fd7b8087
Sayali Kumbhar <63386141+sayali0512@users.noreply.github.com>
2020-07-10 16:13:39 +0530

    changed the parameter structure

--- exportToStorage.sh
+++ exportToStorage.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# Parameters
+# Export the storage.tar file to  Azure blob storage account
 rg_name=$1
 storage_name=$2
 container_name=$3
 user_name=$4
-
 file_path=/home/$4/$5
 blob_storage=$5
 storage_key=$6
 
 # upload files to blob
 upload_files(){
-    az storage blob upload --account-name $2 --account-key $6 --container-name $3 --file /home/$4/$5 --name $5
+    az storage blob upload --account-name $storage_name --account-key $6 --container-name $container_name --file /home/$user_name/$blob_storage --name $blob_storage
 }
-upload_files >> /home/azureadmin/asif/storage_logs.txt
+upload_files >> /home/azureadmin/storage_logs.txt
 
