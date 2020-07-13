df3e2b08fbab66f20105f7429e6da9282d3b5ccc
Sayali Kumbhar <63386141+sayali0512@users.noreply.github.com>
2020-07-13 17:09:56 +0530

    Update exportToStorage.sh

--- exportToStorage.sh
+++ exportToStorage.sh
@@ -3,7 +3,7 @@
 storage_name=$1
 lb_name=$2
 vm_name=$3
-container_name=$4
+container_name=$4/$lb_name/$vm_name
 user_name=$5
 file_path=/home/$5/$6
 blob_storage=$6
@@ -17,7 +17,7 @@ echo blob_storage=$6
 
 # upload files to blob
 upload_files(){
-az storage blob upload --account-name $storage_name --container-name $container_name/$lb_name/$vm_name --file /home/$user_name/$blob_storage --name $blob_storage --sas-token "$SAS_token"
+az storage blob upload --account-name $storage_name --container-name $container_name --file /home/$user_name/$blob_storage --name $blob_storage --sas-token "$SAS_token"
 
 }
 
