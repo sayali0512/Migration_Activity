28d536a9f2a1bf8a25f2e03cd8da9e91080c33ff
Sayali Kumbhar <63386141+sayali0512@users.noreply.github.com>
2020-07-13 17:31:00 +0530

    changed account key to sas token

--- importFromStorage.sh
+++ importFromStorage.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # Parameters
-storage_name=$2
-container_name=$3
-user_name=$4
-file_path=/home/$4/$5
-blob_storage=$5
-storage_key=$6
+
+
+storage_name=$1
+lb_name=$2
+vm_name=$3
+container_name=$4/$lb_name/$vm_name
+user_name=$5
+file_path=/home/$5/$6
+blob_storage=$6
+SAS_token=$7
+
 download_content()
 {
-    az storage blob download --account-name $storage_name --container-name $container_name --name $blob_storage --file /home/$user_name/$blob_storage --account-key $storage_key
+    az storage blob download --account-name $storage_name --container-name $container_name --name $blob_storage --file /home/$user_name/$blob_storage --sas-token "$SAS_token"
 }
 download_content >> /home/$user_name/logimport.txt
 
