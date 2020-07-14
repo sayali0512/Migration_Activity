2615a2cd6731ef139ee64ac108ece6a5b5e29fcf
“sayali0512” <skumbhar@italentdigital.com>
2020-07-14 13:33:57 +0530

    added echo for parameters

--- exportToStorage.sh
+++ exportToStorage.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-storage_name=$1
-vm_name=$2
-container_name=$3/$source_dns_name/$vm_name
-user_name=$4
-file_path=/home/$4/$5
-blob_storage=$5
-SAS_token=$6
-storage_dir_path=$7  #/home/azureadmin/storage
-source_dns_name=$8   #lb-5nw26r.eastus.cloudapp.azure.com
-source_db_server_name=$9  #mysql-5nw26r.mysql.database.azure.com
-source_db_login_name=$10  #dbadmin@mysql-5nw26r
-source_db_pass=$11 #db password
-source_db_name=$12  # wordpress
-sql_dump_filename=$13  #database.sql
-wp_path=$14   # /azlamp/html
-wp_data_path=$15    # /azlamp/data
+echo storage_name=$1
+echo vm_name=$2
+echo container_name=$3/$source_dns_name/$vm_name
+echo user_name=$4
+echo file_path=/home/$4/$5
+echo blob_storage=$5
+echo SAS_token=$6
+echo storage_dir_path=$7  #/home/azureadmin/storage
+echo source_dns_name=$8   #lb-5nw26r.eastus.cloudapp.azure.com
+echo source_db_server_name=$9  #mysql-5nw26r.mysql.database.azure.com
+echo source_db_login_name=$10  #dbadmin@mysql-5nw26r
+echo source_db_pass=$11 #db password
+echo source_db_name=$12  # wordpress
+echo sql_dump_filename=$13  #database.sql
+echo wp_path=$14   # /azlamp/html
+echo wp_data_path=$15    # /azlamp/data
 
 copysitetostorage(){
 
 sudo mkdir $storage_dir_path
 sudo mkdir $storage_dir_path/site
+echo sudo cp -rf $wp_path/$source_dns_name/ $storage_dir_path/site
 sudo cp -rf $wp_path/$source_dns_name/ $storage_dir_path/site
 }
 
 copydatatostorage(){
 
 sudo mkdir $storage_dir_path/data
+echo sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/data
 sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/data
+
 
 }
 databaseexport(){
