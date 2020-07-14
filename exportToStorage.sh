62739bd5d5de558dd04009f53207f85e7718c34e
“sayali0512” <skumbhar@italentdigital.com>
2020-07-14 11:36:25 +0530

    added application migration script

--- exportToStorage.sh
+++ exportToStorage.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 storage_name=$1
-lb_name=$2
-vm_name=$3
-container_name=$4/$lb_name/$vm_name
-user_name=$5
-file_path=/home/$5/$6
-blob_storage=$6
-SAS_token=$7
-storage_dir_path=$8  #/home/azureadmin/storage
-source_dns_name=$9   #lb-5nw26r.eastus.cloudapp.azure.com
-source_db_server_name=$10  #mysql-5nw26r.mysql.database.azure.com
-source_db_login_name=$11  #dbadmin@mysql-5nw26r
-source_db_pass=$12  #db password
-source_db_name=$13  # wordpress
-sql_dump_filename=$14  #database.sql
-wp_path=$15    # /azlamp/html
-wp_data_path=16    # /azlamp/data
+vm_name=$2
+container_name=$3/$source_dns_name/$vm_name
+user_name=$4
+file_path=/home/$4/$5
+blob_storage=$5
+SAS_token=$6
+storage_dir_path=$7  #/home/azureadmin/storage
+source_dns_name=$8   #lb-5nw26r.eastus.cloudapp.azure.com
+source_db_server_name=$9  #mysql-5nw26r.mysql.database.azure.com
+source_db_login_name=$10  #dbadmin@mysql-5nw26r
+source_db_pass=$11 #db password
+source_db_name=$12  # wordpress
+sql_dump_filename=$13  #database.sql
+wp_path=$14   # /azlamp/html
+wp_data_path=$15    # /azlamp/data
 
 copysitetostorage(){
 
