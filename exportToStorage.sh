063b8b52232f1df6e8f90f6e092c7a7ee45c4553
“sayali0512” <skumbhar@italentdigital.com>
2020-07-14 14:21:31 +0530

    removed database function

--- exportToStorage.sh
+++ exportToStorage.sh
@@ -9,13 +9,8 @@ echo blob_storage=$5
 echo SAS_token=$6
 echo storage_dir_path=$7  #/home/azureadmin/storage
 echo source_dns_name=$8   #lb-5nw26r.eastus.cloudapp.azure.com
-echo source_db_server_name=$9  #mysql-5nw26r.mysql.database.azure.com
-echo source_db_login_name=$10  #dbadmin@mysql-5nw26r
-echo source_db_pass=$11 #db password
-echo source_db_name=$12  # wordpress
-echo sql_dump_filename=$13  #database.sql
-echo wp_path=$14   # /azlamp/html
-echo wp_data_path=$15    # /azlamp/data
+echo wp_path=$9   # /azlamp/html
+echo wp_data_path=${10}    # /azlamp/data
 
 copysitetostorage(){
 
@@ -33,12 +28,6 @@ sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/data
 
 
 }
-databaseexport(){
-
-sudo apt-get install mysql-server
-sudo mysqldump -h $source_db_server_name -u $source_db_login_name -p$source_db_pass $source_db_name > $storage_dir_path/$sql_dump_filename
-
-}
 createstoragezip(){
 
 tar cf $storage_dir_path.tar $storage_dir_path/
@@ -50,8 +39,8 @@ az storage blob upload --account-name $storage_name --container-name $container_
 
 }
 
-copysitetostorage >> /home/azureadmin/storage_logs.txt
-copydatatostorage >> /home/azureadmin/storage_logs.txt
+copysitetostorage >> /tmp/storage_logs.txt
+copydatatostorage >> /tmp/storage_logs.txt
 databaseexport >> /home/azureadmin/storage_logs.txt
 createstoragezip >> /home/azureadmin/storage_logs.txt
 upload_files >> /home/azureadmin/storage_logs.txt
