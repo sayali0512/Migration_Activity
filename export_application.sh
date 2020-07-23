6abc5f53ef769a7d823d32f8ca5a7c544983ed47
“sayali0512” <skumbhar@italentdigital.com>
2020-07-23 16:45:55 +0530

    update function

--- export_application.sh
+++ export_application.sh
@@ -4,7 +4,7 @@ storage_name=$1                   # storage account name
 container_name=$2                 # container name
 user_name=$3                      # user name
 file_path=/home/$3/$4.tar         # tar file path /home/azureadmin/storage.tar  $4=storage.tar
-blob_storage=$6/$HOSTNAME/$4.tar  # blob file name /vmname/
+blob_storage=home/$3/$4.tar  # blob file name /vmname/
 SAS_token=$5                      # SAS token
 storage_dir_path=/home/$3/$4      # storage folder path /home/azureadmin/storage $4=storage
 source_dns_name=$6                # dns name
@@ -48,7 +48,7 @@ install_azcopy(){
 
 upload_files(){
     echo "upload_files"
-    sudo azcopy copy '$file_path' 'https://$storage_name.blob.core.windows.net/$container_name/$$SAS_token'
+    sudo azcopy copy '$file_path' 'https://$storage_name.blob.core.windows.net/$container_name/$SAS_token'
 }
 
 copysitetostorage >> /tmp/storage_logs.txt
