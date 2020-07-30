221317d74be53a9bb71e797f8d539b003f28bbf4
“sayali0512” <skumbhar@italentdigital.com>
2020-07-30 12:24:39 +0530

    updated code

--- export_application.sh
+++ export_application.sh
@@ -6,28 +6,28 @@ echo user_name=$3                      # user name
 echo file_path=/home/$3/$4.tar         # tar file path /home/azureadmin/storage.tar  $4=storage.tar
 echo blob_storage=$4.tar  # blob file name /vmname/
 echo SAS_token=$5                      # SAS token
-echo storage_dir_path=/home/$3/$4      # storage folder path /home/azureadmin/storage $4=storage
-echo source_dns_name=$6                # dns name
+echo storageDirPath=/home/$3/$4      # storage folder path /home/azureadmin/storage $4=storage
+echo sourceDnsName=$6                # dns name
 echo wp_path=$7                        # azlamp/html
 echo wp_data_path=$8                   # azlamp/data
 
 copysitetostorage(){
     echo "copysitetostorage"
-    sudo mkdir -p "$storage_dir_paths"
-    sudo mkdir -p "$storage_dir_path"/"$source_dns_name"
-    sudo mkdir -p "$storage_dir_path"/"$source_dns_name"/site
-    sudo cp -rf $wp_path/$source_dns_name/ $storage_dir_path/$source_dns_name/site
+    sudo mkdir -p "$storageDirPath"
+    sudo mkdir -p "$storageDirPath"/"$sourceDnsName"
+    sudo mkdir -p "$storageDirPath"/"$sourceDnsName"/site
+    sudo cp -rf $wp_path/$sourceDnsName/ $storageDirPath/$sourceDnsName/site
 }
 
 copydatatostorage(){
     echo "copydatatostorage"
-    sudo mkdir -p  "$storage_dir_path"/"$source_dns_name"/data
-    sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/$source_dns_name/data
+    sudo mkdir -p  "$storageDirPath"/"$sourceDnsName"/data
+    sudo cp -rf $wp_data_path/$sourceDnsName $storageDirPath/$sourceDnsName/data
 }
 
 createstoragetar(){
     echo "create storage tar"
-    tar cf $storage_dir_path.tar $storage_dir_path
+    tar cf $storageDirPath.tar $storageDirPath
 }
 
 create_container(){
@@ -48,7 +48,7 @@ install_azcopy(){
 
 upload_files(){
     echo "upload_files"
-    sudo azcopy copy '$file_path' 'https://$storage_name.blob.core.windows.net/container_name/$source_dns_name/$SAS_token'
+    sudo azcopy copy '$file_path' 'https://$storage_name.blob.core.windows.net/container_name/$sourceDnsName/$SAS_token'
 }
 
 copysitetostorage >> /tmp/storage_logs.txt
