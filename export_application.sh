fa8d94b87b470dfc4f6f37654f353ea8c95aa5ce
“sayali0512” <skumbhar@italentdigital.com>
2020-07-30 14:13:42 +0530

    updated code

--- export_application.sh
+++ export_application.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 storage_name=$1                   # storage account name
-container_name=$2/$6                 # container name
+container_name=$2                 # container name
 user_name=$3                      # user name
 file_path=/home/$3/$4.tar         # tar file path /home/azureadmin/storage.tar  $4=storage.tar
-blob_storage=$6/$HOSTNAME/$4.tar  # blob file name /vmname/
+blob_storage=$6/$4.tar  # blob file name /vmname/
 SAS_token=$5                      # SAS token
 storage_dir_path=/home/$3/$4      # storage folder path /home/azureadmin/storage $4=storage
 source_dns_name=$6                # dns name
 wp_path=$7                        # azlamp/html
 wp_data_path=$8                   # azlamp/data
+vm_name=$9
+
 
 copysitetostorage(){
     echo "copysitetostorage"
     sudo mkdir $storage_dir_path
-    sudo mkdir $storage_dir_path/$HOSTNAME
-    sudo mkdir $storage_dir_path/$HOSTNAME/site
-    sudo cp -rf $wp_path/$source_dns_name/ $storage_dir_path/$HOSTNAME/site
+    sudo mkdir $storage_dir_path/site
+    sudo cp -rf $wp_path/$source_dns_name/ $storage_dir_path/site
 }
 
 copydatatostorage(){
     echo "copydatatostorage"
-    sudo mkdir $storage_dir_path/$HOSTNAME/data
-    sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/$HOSTNAME/data
+    sudo mkdir $storage_dir_path/data
+    sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/data
 }
 
 createstoragetar(){
@@ -32,7 +33,7 @@ createstoragetar(){
 
 create_container(){
     echo "create_container"
-    az storage container create --account-name $storage_name --name $container_name --sas-token $SAS_token
+    az storage container create --account-name $storage_name --name $container_name/$source_dns_name --sas-token $SAS_token
 }
 
 install_azcopy(){
@@ -49,7 +50,7 @@ install_azcopy(){
 
 upload_files(){
     echo "upload_files"
-     sudo azcopy copy '/home/azureadmin/storage.tar' 'https://abs5nw26r.blob.core.windows.net/migration/lb-5nw26r.eastus.cloudapp.azure.com/?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-07-30T14:32:28Z&st=2020-07-30T06:32:28Z&spr=https&sig=BktAkNlQCtqh8I307hqO2eBXtzeT6g%2Fp8TGurjktgIE%3D'
+     sudo azcopy copy 'file_path' 'https://$storage_name.blob.core.windows.net/$container_name/$source_dns_name/$SAS_token'
 
 }
 
@@ -57,5 +58,5 @@ copysitetostorage >> /tmp/storage_logs.txt
 copydatatostorage >> /tmp/storage_logs.txt
 createstoragetar >> /tmp/storage_logs.txt
 create_container >> /tmp/storage_logs.txt
-install_azcopy  >> /tmp/storage_logs.txt
+#install_azcopy  >> /tmp/storage_logs.txt
 upload_files >> /tmp/storage_logs.txt
