98ccf4ea27d059ddecbd6acbbd17f59c75c19b83
“sayali0512” <skumbhar@italentdigital.com>
2020-07-30 12:46:30 +0530

    updated code

--- export_application.sh
+++ export_application.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
-echo storage_name=$1                   # storage account name
-echo container_name=$2                 # container name
-echo user_name=$3                      # user name
-echo file_path=/home/$3/$4.tar         # tar file path /home/azureadmin/storage.tar  $4=storage.tar
-echo blob_storage=$4.tar  # blob file name /vmname/
-echo SAS_token=$5                      # SAS token
-echo storageDirPath=/home/$3/$4      # storage folder path /home/azureadmin/storage $4=storage
-echo sourceDnsName=$6                # dns name
-echo wp_path=$7                        # azlamp/html
-echo wp_data_path=$8                   # azlamp/data
+storage_name=$1                   # storage account name
+container_name=$2                 # container name
+user_name=$3                      # user name
+file_path=/home/$3/$4.tar         # tar file path /home/azureadmin/storage.tar  $4=storage.tar
+blob_storage=$6/$HOSTNAME/$4.tar  # blob file name /vmname/
+SAS_token=$5                      # SAS token
+storage_dir_path=/home/$3/$4      # storage folder path /home/azureadmin/storage $4=storage
+source_dns_name=$6                # dns name
+wp_path=$7                        # azlamp/html
+wp_data_path=$8                   # azlamp/data
 
 copysitetostorage(){
     echo "copysitetostorage"
-    sudo mkdir -p "$storageDirPath"
-    sudo mkdir -p "$storageDirPath"/"$sourceDnsName"
-    sudo mkdir -p "$storageDirPath"/"$sourceDnsName"/site
-    sudo cp -rf $wp_path/$sourceDnsName/ $storageDirPath/$sourceDnsName/site
+    sudo mkdir $storage_dir_path
+    sudo mkdir $storage_dir_path/$HOSTNAME
+    sudo mkdir $storage_dir_path/$HOSTNAME/site
+    sudo cp -rf $wp_path/$source_dns_name/ $storage_dir_path/$HOSTNAME/site
 }
 
 copydatatostorage(){
     echo "copydatatostorage"
-    sudo mkdir -p  "$storageDirPath"/"$sourceDnsName"/data
-    sudo cp -rf $wp_data_path/$sourceDnsName $storageDirPath/$sourceDnsName/data
+    sudo mkdir $storage_dir_path/$HOSTNAME/data
+    sudo cp -rf $wp_data_path/$source_dns_name $storage_dir_path/$HOSTNAME/data
 }
 
 createstoragetar(){
     echo "create storage tar"
-    tar cf $storageDirPath.tar $storageDirPath
+    tar cf $storage_dir_path.tar $storage_dir_path
 }
 
 create_container(){
     echo "create_container"
     az storage container create --account-name $storage_name --name $container_name --sas-token $SAS_token
 }
+
 install_azcopy(){
     #Download AzCopy
     wget https://aka.ms/downloadazcopy-v10-linux
@@ -55,5 +56,4 @@ copysitetostorage >> /tmp/storage_logs.txt
 copydatatostorage >> /tmp/storage_logs.txt
 createstoragetar >> /tmp/storage_logs.txt
 create_container >> /tmp/storage_logs.txt
-install_azcopy  >> /tmp/storage_logs.txt
 upload_files >> /tmp/storage_logs.txt
