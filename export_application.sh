5d325e6e8ae988a1d825ab63ef1b62d2fe42e892
“sayali0512” <skumbhar@italentdigital.com>
2020-07-30 15:34:17 +0530

    updated code

--- export_application.sh
+++ export_application.sh
@@ -50,7 +50,7 @@ install_azcopy(){
 upload_files(){
     echo "upload_files"
    #az storage blob upload --account-name $storage_name --container-name $container_name --file $file_path --name $blob_storage --sas-token $SAS_token
-   sudo azcopy copy '$file_path' 'https://$storage_name.blob.core.windows.net/$container_name/$source_dns_name/$SAS_token'
+  sudo azcopy copy '/home/azureadmin/storage.tar' 'https://abs5nw26r.blob.core.windows.net/migration/lb-5nw26r.eastus.cloudapp.azure.com/?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-07-30T14:32:28Z&st=2020-07-30T06:32:28Z&spr=https&sig=BktAkNlQCtqh8I307hqO2eBXtzeT6g%2Fp8TGurjktgIE%3D'
 
 
 }
